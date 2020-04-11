c5bd832c8cb6a7512063a3789dabd8e47abc71f3
Naruenart Khomut <51041422+naruenrtkhomut@users.noreply.github.com>
2020-04-11 21:52:09 +0700

    Add files via upload

---
+++ Models/session_data.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleWebFrontend_ASP_NETCORE31.Models
+{
+    public class session_data
+    {
+        public const string Session_Order_Name = "_OrderName";
+    }
+}
